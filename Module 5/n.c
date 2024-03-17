7163668e2d56df68b73d294fbf00d16e9db282af
Jahin Tajowar Masud <148520133+JahinTajowar@users.noreply.github.com>
2024-03-17 22:28:31 +0600

    Add files via upload

---
+++ Module 5/n.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a>='a' && a<='z')
+    {
+        // choto hater
+        int ans=a-32;
+        printf("%c",ans);
+    }
+    else 
+    {
+        // boro hater
+        int ans=a+32;
+        printf("%c",ans);
+    }
+    return 0;
+}
